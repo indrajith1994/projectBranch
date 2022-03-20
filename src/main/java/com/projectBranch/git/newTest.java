fed1198e79dd65698064c4248fd5d5162e8a1be1
indrajit <indrajith.1994@gmail.com>
2022-03-20 13:30:53 +0530

    Create newTest.java

---
+++ src/main/java/com/projectBranch/git/newTest.java
@@ -0,0 +1,13 @@
+package com.projectBranch.git;
+
+/**
+ * Hello world!
+ *
+ */
+public class newTest 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
